d44e1a8d9221a5b7bc685f43ef7cf164daa4b3b9
Randy T <randythai360@gmail.com>
2023-09-03 14:21:24 -0500

    backend wip2

--- backend/index.js
+++ backend/index.js
@@ -1,45 +1,63 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
+// const express = require('express');
+// const app = express();
+// const cors = require('cors');
 const mysql = require('mysql2');
 
-app.use(cors());
-app.use(express.json());
-
-const db = mysql.createConnection({
-    user: 'root',
-    host: 'localhost',
-    password: '7263990306750Rndy,./',
-    database: 'emailDB'
-});
-
-app.post('/send', (req, res) => {
-    const email = req.body.email;
-
-    db.query("INSERT INTO emails (email) VALUES (?)",
-    [email],
-    (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.send("Email added successfully.");
-        }
-    });
-});
-
-app.get('/grabEmails', (req, res) => {
-    console.log("Attempting to Grab...");
-    db.query("SELECT * FROM emails",
-    (err, data) => {
-        if (err) {
-            console.log(err);
-        }else{
-            console.log("Emails retrieved.");
-            res.send(data);
-        }
-    });
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DBNAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 })
 
-app.listen(3001, () => {
-    console.log("Server running on port 3001.");
+pool.getConnection((err, connection) => {
+    if(err) console.log(err)
+    console.log("Connected Successfully");
 })
+
+module.exports = pool.promise()
+
+// app.use(cors());
+// app.use(express.json());
+
+// const db = mysql.createConnection({
+//     user: 'root',
+//     host: 'localhost',
+//     password: '7263990306750Rndy,./',
+//     database: 'emailDB',
+//     port: 3001
+// });
+
+// app.post('/send', (req, res) => {
+//     const email = req.body.email;
+
+//     db.query("INSERT INTO emails (email) VALUES (?)",
+//     [email],
+//     (err, result) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             res.send("Email added successfully.");
+//         }
+//     });
+// });
+
+// app.get('/grabEmails', (req, res) => {
+//     console.log("Attempting to Grab...");
+//     db.query("SELECT * FROM emails",
+//     (err, data) => {
+//         if (err) {
+//             console.log(err);
+//         }else{
+//             console.log("Emails retrieved.");
+//             res.send(data);
+//         }
+//     });
+// })
+
+// app.listen(3001, () => {
+//     console.log("Server running on port 3001.");
+// })
